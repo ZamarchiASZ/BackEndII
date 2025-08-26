7b7900f7e5062325bf989b671e099a6c639ce179
ZamarchiASZ <zamarchi.a@estudante.ifro.edu.br>
2025-08-26 08:44:55 -0400

    haikyuu

---
+++ 1BIM/tarefas/dados.js
@@ -0,0 +1,56 @@
+const colecaoHaikyuu = [
+    {
+        id: 1,
+        ano: "2014",
+        nome: "Haikyuu!!",
+        episodios: "25" 
+    },
+    {
+        id: 2,
+        ano: "2015",
+        nome: "Haikyuu!! Second Season",
+        episodios: "25" 
+    },   
+    {
+        id: 3,
+        ano: "2016",
+        nome: "Haikyuu!! Karasuno Koukou vs. Shiratorizawa Gakuen Koukou",
+        episodios: "10" 
+    },  
+    {
+        id: 4,
+        ano: "2020",
+        nome: "Haikyuu!! To The Top (Parte 1)",
+        episodios: "13" 
+    },    {
+        id: 5,
+        ano: "2020",
+        nome: "Haikyuu!!: To The Top (Parte 2)",
+        episodios: "12" 
+    },
+    {
+        id: 6,
+        ano: "2015",
+        nome: "Haikyuu!!: Lev Genzan!",
+        formato: "Filme"
+    },
+    {
+        id: 7,
+        ano: "2016",
+        nome: "Haikyuu!! vs. Akaten",
+        formato: "Filme" 
+    },
+    {
+        id: 8,
+        ano: "2017",
+        nome: "Haikyuu!!: Tokushuu! Haru-kou Volley ni Kaketa Seishun",
+        formato: "Filme"
+    },
+    {
+        id: 9,
+        ano: "2020",
+        nome: "Haikyuu!!: Riku vs. Kuu",
+        formato: "Filme" 
+    },
+]
+export default colecaoHaikyuu;
---
+++ 1BIM/tarefas/inicio.js
@@ -0,0 +1,34 @@
+import colecaoHaikyuu from "../tarefas/dados.js";
+import express from 'express';
+
+const app = express();
+
+app.get('/Haikyuu', (req, res) =>  {
+    res.json(colecaoHaikyuu);
+});
+
+app.get('/haikyuu/:idHai', (req, res) => {
+    const idHai = parseInt(req.params.idHai);
+    let mensagemErro = '';
+    let hai;
+
+    if (!isNaN(idHai)) {
+     hai = colecaoHaikyuu.find(u => u.id === idHai);
+     if (!hai) {
+        mensagemErro = "Filme ou temporada não encontrada";
+     }   
+    }else {
+        mensagemErro = 'Número iválida';
+    }
+    if (hai) {
+        res.json(hai);
+    }else {
+        res.status(404).json({ "erro": mensagemErro})
+    }
+
+});
+
+app.listen(8080, () => {
+    let data = new Date();
+    console.log('Servidor iniciado na porta 8080 em: ' + data);
+});
